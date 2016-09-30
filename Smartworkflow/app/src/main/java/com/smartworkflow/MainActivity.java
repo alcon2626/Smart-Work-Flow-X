9fa6cccc6d3907696425a51d0e5c999cd787f55c
Leonel Gonzalez <alcon2626@gmail.com>
2016-09-30 06:45:32 -0400

    back press takes the app home. Done!

--- Smartworkflow/app/src/main/java/com/smartworkflow/MainActivity.java
+++ Smartworkflow/app/src/main/java/com/smartworkflow/MainActivity.java
@@ -169,7 +169,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     @Override
     public void onBackPressed(){
         if (exit) {
-            finish(); // finish activity
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         } else {
             Toast.makeText(this, "Press Back again to Exit.",
                     Toast.LENGTH_SHORT).show();
